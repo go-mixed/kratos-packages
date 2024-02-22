c05ef821482871c4e4ea7fa4490004e9a2291b65
dxg <dxg@foxmail.com>
2024-02-22 21:03:14 +0800

    add func of event-stream to stream

--- pkg/http/stream/stream.go
+++ pkg/http/stream/stream.go
@@ -3,6 +3,8 @@ package stream
 import (
 	kratosHttp "github.com/go-kratos/kratos/v2/transport/http"
 	"io"
+	"strconv"
+	"time"
 )
 
 type Stream struct {
@@ -35,12 +37,6 @@ func (s *Stream) Close() error {
 	}
 }
 
-// WriteAndClose writes the data to the stream and closes the stream. MUST-run in a separate goroutine.
-func (s *Stream) WriteAndClose(data []byte) error {
-	_, _ = s.Write(data)
-	return s.Close()
-}
-
 // Write writes the data to the stream. MUST-run in a separate goroutine.
 func (s *Stream) Write(data []byte) (int, error) {
 	n, err := s.pipeWriter.Write(data)
@@ -48,6 +44,47 @@ func (s *Stream) Write(data []byte) (int, error) {
 		s.Close()
 	}
 	return n, err
+}
+
+func (s *Stream) WriteString(val string) (int, error) {
+	return s.Write([]byte(val))
+}
+
+func (s *Stream) WriteEvent(event string) error {
+	_, err := s.WriteString("event: " + event + "\n")
+	return err
+}
+
+func (s *Stream) WriteData(data string) error {
+	_, err := s.WriteString("data: " + data + "\n\n")
+	return err
+}
+
+func (s *Stream) WriteId(id string) error {
+	_, err := s.WriteString("id: " + id + "\n")
+	return err
+}
+
+func (s *Stream) WriteRetry(retry time.Duration) error {
+	_, err := s.WriteString("id: " + strconv.FormatInt(retry.Milliseconds(), 10) + "\n")
+	return err
+}
+
+func (s *Stream) WriteComment(comment string) error {
+	_, err := s.WriteString(": " + comment + "\n")
+	return err
+}
+
+// WriteAndClose writes the data to the stream and closes the stream. MUST-run in a separate goroutine.
+func (s *Stream) WriteAndClose(data []byte) error {
+	_, _ = s.Write(data)
+	return s.Close()
+}
+
+// WriteStringAndClose writes the data to the stream and closes the stream. MUST-run in a separate goroutine.
+func (s *Stream) WriteStringAndClose(data string) error {
+	_, _ = s.WriteString(data)
+	return s.Close()
 }
 
 func (s *Stream) Wait() error {
