33984cb3842d7a50692ec58cc836c3ef1ea54455
dxg <dxg@foxmail.com>
2024-03-02 21:13:29 +0800

    change GetAndValidate to optional arguments

--- pkg/auth/context.go
+++ pkg/auth/context.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"github.com/go-kratos/kratos/v2/errors"
+	"slices"
 )
 
 type authKey struct{}
@@ -17,12 +18,13 @@ func FromContext(ctx context.Context) (IAuth, bool) {
 	return info, ok
 }
 
-func GetAndValidate(ctx context.Context, guardName string) (IAuth, error) {
+// GetAndValidate get auth info from context and validate the guard name
+func GetAndValidate(ctx context.Context, guardNames ...string) (IAuth, error) {
 	session, ok := FromContext(ctx)
 	if !ok {
 		return nil, errors.Unauthorized("", "Please get the access token first")
-	} else if session.GetGuardModel().GetGuardName() != guardName {
-		return nil, errors.Forbidden(guardName, "The guard name of access token is not matched")
+	} else if !slices.Contains(guardNames, session.GetGuardModel().GetGuardName()) {
+		return nil, errors.Forbidden("", "The guard name of access token is not matched")
 	}
 
 	return session, nil
