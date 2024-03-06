2e366f705fc97b8aebb64cfb91a1c09375d2c0b1
dxg <dxg@foxmail.com>
2024-03-06 18:56:10 +0800

    add PathToURI

--- pkg/utils/io.go
+++ pkg/utils/io.go
@@ -3,10 +3,30 @@ package utils
 import (
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // GetCurrentDir get current dir of the executable
 func GetCurrentDir() string {
 	path, _ := os.Executable()
 	return filepath.Dir(path)
+}
+
+// PathToURI Path to file uri
+func PathToURI(path string) string {
+	// convert to the absolute path
+	if !filepath.IsAbs(path) {
+		if _path, err := filepath.Abs(path); err == nil {
+			path = _path
+		}
+	}
+
+	// convert to unix style
+	path = filepath.ToSlash(path)
+	// convert a windows' driver to lower case, and remove the colon: "C:" -> "/c"
+	if len(path) >= 2 && path[1] == ':' && ((path[0] >= 'A' && path[0] <= 'Z') || (path[0] >= 'a' && path[0] <= 'z')) {
+		path = "/" + strings.ToLower(path[0:1]) + path[2:]
+	}
+
+	return "file://" + path
 }
