e3043be1f00c8ac87303166a58db9a9fc377b2bc
dxg <dxg@foxmail.com>
2024-04-10 10:45:12 +0800

    add http server/client logger

---
+++ pkg/middleware/log/client.go
@@ -0,0 +1,46 @@
+package log
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+	"gopkg.in/go-mixed/kratos-packages.v2/pkg/log"
+	"time"
+)
+
+// Client is a client logging middleware.
+func Client(logger *log.Helper) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			var (
+				code      int32
+				reason    string
+				kind      string
+				operation string
+			)
+			startTime := time.Now()
+			if info, ok := transport.FromClientContext(ctx); ok {
+				kind = info.Kind().String()
+				operation = info.Operation()
+			}
+			reply, err = handler(ctx, req)
+			if se := errors.FromError(err); se != nil {
+				code = se.Code
+				reason = se.Reason
+			}
+			level, stack := extractError(err)
+			_ = logger.WithContext(ctx).Log(level,
+				"kind", "client",
+				"component", kind,
+				"operation", operation,
+				"args", extractArgs(req),
+				"code", code,
+				"reason", reason,
+				"stack", stack,
+				"latency", time.Since(startTime).Seconds(),
+			)
+			return
+		}
+	}
+}
---
+++ pkg/middleware/log/server.go
@@ -0,0 +1,65 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+	"gopkg.in/go-mixed/kratos-packages.v2/pkg/log"
+	"time"
+)
+
+// Server the middleware of http server, record any request and response
+// uses it between registry.Middleware() and validate.Validator()
+func Server(logger *log.Helper) middleware.Middleware {
+	return func(nextHandler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			var (
+				code      int32
+				reason    string
+				kind      string
+				operation string
+			)
+			startTime := time.Now()
+			if info, ok := transport.FromServerContext(ctx); ok {
+				kind = info.Kind().String()
+				operation = info.Operation()
+			}
+			reply, err = nextHandler(ctx, req)
+			if se := errors.FromError(err); se != nil {
+				code = se.Code
+				reason = se.Reason
+			}
+			level, stack := extractError(err)
+			_ = logger.WithContext(ctx).Log(level,
+				"kind", "server",
+				"component", kind,
+				"operation", operation,
+				"args", extractArgs(req),
+				"code", code,
+				"reason", reason,
+				"stack", stack,
+				"latency", time.Since(startTime).Seconds(),
+			)
+			return
+
+		}
+	}
+}
+
+// extractArgs returns the string of the req
+func extractArgs(req interface{}) string {
+	if stringer, ok := req.(fmt.Stringer); ok {
+		return stringer.String()
+	}
+	return fmt.Sprintf("%+v", req)
+}
+
+// extractError returns the string of the error
+func extractError(err error) (log.Level, string) {
+	if err != nil {
+		return log.LevelError, fmt.Sprintf("%+v", err)
+	}
+	return log.LevelInfo, ""
+}
