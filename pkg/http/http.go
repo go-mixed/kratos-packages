6ebf5fac4d1f6e597415ba668e2421b726f02ede
dxg <dxg@foxmail.com>
2024-08-22 17:58:23 +0800

    set ResponseHeaderTimeout to DefaultHttpClient

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -5,9 +5,13 @@ import (
 	"time"
 )
 
+// DefaultHttpClient 返回默认httpClient
+//   - timeout: 等待Header响应头，以及总超时时间
 func DefaultHttpClient(timeout time.Duration) *http.Client {
+	options := DefaultTransportOptions()
+	options.ResponseHeaderTimeout = timeout
 	return &http.Client{
 		Timeout:   timeout,
-		Transport: NewHttpTransport(DefaultTransportOptions()),
+		Transport: NewHttpTransport(options),
 	}
 }
