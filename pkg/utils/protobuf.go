518bb06e8d9990570fad9b8a5ab8b0a03aa0df9f
dxg <dxg@foxmail.com>
2024-11-28 14:10:25 +0800

    add GetProtoBufField, SortProtobufList, CompareProtoBuf to protobuf

--- pkg/utils/protobuf.go
+++ pkg/utils/protobuf.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"bytes"
+	"cmp"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"reflect"
 	"strings"
@@ -52,4 +54,60 @@ func ProtobufToMap(protobuf IProtobuf, keepNil bool) map[string]any {
 
 	}
 	return results
+}
+
+func GetProtoBufField[P IProtobuf](message P, fieldName string) protoreflect.FieldDescriptor {
+	messageDesc := message.ProtoReflect().Descriptor()
+	return messageDesc.Fields().ByName(protoreflect.Name(fieldName))
+}
+
+// CompareProtoBuf 比较两个protobuf消息，
+// message1 < message2 返回 -1，message1 > message2 返回 1，message1 == message2 返回 0
+func CompareProtoBuf[P IProtobuf](message1 P, message2 P, fieldName string) int {
+	field1 := GetProtoBufField(message1, fieldName)
+	field2 := GetProtoBufField(message2, fieldName)
+
+	if field1 == nil || field2 == nil { // field1 == field2
+		return 0
+	}
+
+	kind := field1.Kind()
+	if kind != protoreflect.StringKind &&
+		kind != protoreflect.BytesKind &&
+		kind != protoreflect.Int32Kind &&
+		kind != protoreflect.Int64Kind &&
+		kind != protoreflect.Uint32Kind &&
+		kind != protoreflect.Uint64Kind &&
+		kind != protoreflect.FloatKind &&
+		kind != protoreflect.DoubleKind &&
+		kind != protoreflect.BoolKind &&
+		kind != protoreflect.Sint32Kind &&
+		kind != protoreflect.Sint64Kind &&
+		kind != protoreflect.Fixed32Kind &&
+		kind != protoreflect.Fixed64Kind &&
+		kind != protoreflect.Sfixed32Kind &&
+		kind != protoreflect.Sfixed64Kind {
+		// 只支持字符串、数字、布尔等类型，其它类型返回0
+		return 0
+	}
+
+	val1 := message1.ProtoReflect().Get(field1)
+	val2 := message2.ProtoReflect().Get(field2)
+
+	switch kind {
+	case protoreflect.StringKind:
+		return strings.Compare(val1.String(), val2.String())
+	case protoreflect.BytesKind:
+		return bytes.Compare(val1.Bytes(), val2.Bytes())
+	case protoreflect.BoolKind:
+		return If(val1.Bool(), 1, 0) - If(val2.Bool(), 1, 0)
+	case protoreflect.Int32Kind, protoreflect.Int64Kind, protoreflect.Sint32Kind, protoreflect.Sint64Kind,
+		protoreflect.Fixed32Kind, protoreflect.Fixed64Kind, protoreflect.Sfixed32Kind, protoreflect.Sfixed64Kind:
+		return cmp.Compare(val1.Int(), val2.Int())
+	case protoreflect.Uint32Kind, protoreflect.Uint64Kind:
+		return cmp.Compare(val1.Uint(), val2.Uint())
+	case protoreflect.FloatKind, protoreflect.DoubleKind: // 这里在Goland中会提示Condition is always true，因为它识别到上面的if条件排除了其它类型，不用管这个问题
+		return cmp.Compare(val1.Float(), val2.Float())
+	}
+	return 0
 }
--- pkg/utils/sort.go
+++ pkg/utils/sort.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"sort"
+	"strings"
 )
 
 // SortSliceBy sorts a slice of any type by a slice of comparable, the order of which is given by the second slice
@@ -29,5 +31,49 @@ func SortSliceBy[S any, T comparable](originalList S, sortedBy []T, fn func(i in
 		}
 
 		return false
+	})
+}
+
+// SortProtobufList 对Protobuf List进行排序，
+//
+//   - orderField: protobuf文件中Message的字段，比如：Message{int32 xx = 1;}中的字段xx
+//   - orderType: "asc" or "desc"
+func SortProtobufList[P IProtobuf](protoMessageList []P, orderField string, orderType string) {
+	orderType = strings.ToLower(orderType)
+	if orderType != "desc" && orderType != "asc" {
+		orderType = "asc"
+	}
+
+	var message P
+	field := GetProtoBufField(message, orderField)
+	if field == nil { // 字段不存在，不做排序
+		return
+	}
+	kind := field.Kind()
+	if kind != protoreflect.StringKind &&
+		kind != protoreflect.BytesKind &&
+		kind != protoreflect.Int32Kind &&
+		kind != protoreflect.Int64Kind &&
+		kind != protoreflect.Uint32Kind &&
+		kind != protoreflect.Uint64Kind &&
+		kind != protoreflect.FloatKind &&
+		kind != protoreflect.DoubleKind &&
+		kind != protoreflect.BoolKind &&
+		kind != protoreflect.Sint32Kind &&
+		kind != protoreflect.Sint64Kind &&
+		kind != protoreflect.Fixed32Kind &&
+		kind != protoreflect.Fixed64Kind &&
+		kind != protoreflect.Sfixed32Kind &&
+		kind != protoreflect.Sfixed64Kind {
+		// 只支持字符串、数字、布尔等类型
+		return
+	}
+
+	sort.Slice(protoMessageList, func(i, j int) bool {
+		cmp := CompareProtoBuf(protoMessageList[i], protoMessageList[j], orderField)
+		if orderType == "asc" { // 升序时，i < j 返回true
+			return cmp < 0
+		}
+		return cmp > 0
 	})
 }
